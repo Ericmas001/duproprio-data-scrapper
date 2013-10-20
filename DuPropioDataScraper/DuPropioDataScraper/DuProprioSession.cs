66321a5bbb1da88ffa70d3aefefb060add5bc221
ericmas001@gmail.com <ericmas001@gmail.com@20a91c41-d005-c3a4-72c9-c4ccf7bf77af>
2013-10-20 17:35:40 +0000

    Scraping summary

---
+++ DuPropioDataScraper/DuPropioDataScraper/DuProprioSession.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using EricUtility;
+using System.Drawing;
+using System.IO;
+using EricUtility.Networking.Gathering;
+
+namespace DuPropioDataScraper
+{
+    public class DuProprioSession
+    {
+        private string m_Username;
+        private string m_Password;
+
+        private CookieContainer m_Cookies;
+        private HttpClient m_Client;
+
+        private bool m_Connected = false;
+
+        public bool Connected
+        {
+            get { return m_Connected; }
+        }
+
+        public DuProprioSession(string user, string pass)
+        {
+            m_Username = user;
+            m_Password = pass;
+        }
+
+        private async Task<bool> Connect()
+        {
+            m_Cookies = new CookieContainer();
+            m_Client = new HttpClient(new HttpClientHandler() { CookieContainer = m_Cookies });
+
+            string URI = "http://duproprio.com/webservice/account/validate-login/";
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, URI);
+
+            request.Headers.Host = "duproprio.com";
+
+            //Accept: text/plain, */*; q=0.01
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*", 0.01));
+
+            //Accept-Language: fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3
+            request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("fr"));
+            request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("fr-fr", 0.8));
+            request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("en-us", 0.5));
+            request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("en", 0.5));
+
+            //Accept-Encoding: gzip, deflate
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("deflate"));
+
+            //Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+            request.Content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("username", m_Username),
+                new KeyValuePair<string, string>("password", m_Password),
+                new KeyValuePair<string, string>("rememberme", "on"),
+            });
+            //request.Content = new StreamContent(new MemoryStream(System.Text.Encoding.UTF8.GetBytes(args)));
+            request.Content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded");
+
+            //User-Agent: Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0
+            request.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
+
+            //X-Requested-With: XMLHttpRequest
+            request.Headers.Add("X-Requested-With", "XMLHttpRequest");
+
+            request.Headers.Referrer = new Uri("http://duproprio.com/modal/account/login");
+
+            HttpResponseMessage result = await m_Client.SendAsync(request);
+
+
+            if (result.IsSuccessStatusCode) 
+                m_Connected = (await m_Client.GetStringAsync("http://duproprio.com")).Contains("<li class=\"logout\">");
+
+            return m_Connected;
+        }
+
+        public async Task<IEnumerable<FavItemSummary>> GetFavs()
+        {
+            if (!m_Connected)
+                await Connect();
+            if (!m_Connected)
+                return null;
+            // Get all favs
+            IEnumerable<string> liste = (await m_Client.GetStringAsync("http://duproprio.com/my-account/dashboard-buyer/bookmarks")).Extract("<ul id=\"favouritesResults\">", "</ul>").Split(new String[] { "</li>" }, StringSplitOptions.None);
+
+            // Trim and delete empty entries
+            liste = liste.Select(x => x.Trim()).Where(x => !string.IsNullOrWhiteSpace(x));
+
+            // Get Rid of the Sold ones
+            liste = liste.Where(x => !x.Contains("<div class=\"infoSold\">"));
+
+            return liste.Select(x => new FavItemSummary(x));
+        }
+    }
+}
---
+++ DuPropioDataScraper/DuPropioDataScraper/FavItemSummary.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EricUtility;
+using System.Globalization;
+using System.Web;
+using System.Drawing;
+using EricUtility.Networking.Gathering;
+using System.Net.Http;
+
+namespace DuPropioDataScraper
+{
+    public class FavItemSummary
+    {
+        private int m_NoAnnonce;
+        private double m_Latitude;
+        private double m_Longitude;
+        private string m_DetailsURL;
+        private string m_Type;
+        private string m_Ville;
+        private string m_Region;
+        private string m_Adresse;
+        private string m_ImageURL;
+        private int m_Prix;
+        private string m_Precison;
+        private string m_Summary;
+
+        public int NoAnnonce
+        {
+            get { return m_NoAnnonce; }
+            set { m_NoAnnonce = value; }
+        }
+
+        public double Latitude
+        {
+            get { return m_Latitude; }
+            set { m_Latitude = value; }
+        }
+
+        public double Longitude
+        {
+            get { return m_Longitude; }
+            set { m_Longitude = value; }
+        }
+
+        public string DetailsURL
+        {
+            get { return m_DetailsURL; }
+            set { m_DetailsURL = value; }
+        }
+
+        public string Type
+        {
+            get { return m_Type; }
+            set { m_Type = value; }
+        }
+
+        public string Ville
+        {
+            get { return m_Ville; }
+            set { m_Ville = value; }
+        }
+
+        public string Region
+        {
+            get { return m_Region; }
+            set { m_Region = value; }
+        }
+
+        public string Adresse
+        {
+            get { return m_Adresse; }
+            set { m_Adresse = value; }
+        }
+
+        public string ImageURL
+        {
+            get { return m_ImageURL; }
+            set { m_ImageURL = value; }
+        }
+
+        public int Prix
+        {
+            get { return m_Prix; }
+            set { m_Prix = value; }
+        }
+
+        public string Precison
+        {
+            get { return m_Precison; }
+            set { m_Precison = value; }
+        }
+
+        public string Summary
+        {
+            get { return m_Summary; }
+            set { m_Summary = value; }
+        }
+
+        public string GMapURL
+        {
+            get { return String.Format(CultureInfo.InvariantCulture, "https://maps.google.com/maps?q={0},{1}", m_Latitude, m_Longitude); }
+        }
+
+        public override string ToString()
+        {
+            return "#" + m_NoAnnonce + " - " + m_Prix.ToString("C0") + " - " + m_Ville + ", " + m_Type + ", " + m_Adresse;
+        }
+
+        public FavItemSummary(string html)
+        {
+            m_NoAnnonce = int.Parse(html.Extract("data-code=\"", "\"").Trim());
+
+            string[] latLong = html.Extract("rel=\"", "\"").Split('|');
+            m_Latitude = double.Parse(latLong[0].Trim(), NumberStyles.Any, NumberFormatInfo.InvariantInfo);
+            m_Longitude = double.Parse(latLong[1].Trim(), NumberStyles.Any, NumberFormatInfo.InvariantInfo);
+
+            string firstLink = html.Extract("<a",">");
+            m_DetailsURL = "http://duproprio.com" + firstLink.Extract("href=\"", "\"");
+
+            string title = firstLink.Extract("title=\"", "\"");
+            int iRegion = title.IndexOf(",") +1 ;
+            int iVille = title.LastIndexOf('à', iRegion);
+            int iAdresse = title.IndexOf(",", iRegion);
+
+            m_Type = title.Remove(iVille).Trim();
+            m_Ville = title.Substring(iVille + 1, (iRegion - iVille) -2).Trim();
+            m_Region = title.Substring(iRegion, iAdresse - iRegion).Trim();
+            m_Adresse = HttpUtility.HtmlDecode(title.Substring(iAdresse + 1).Replace("., ","").Trim());
+
+            m_ImageURL = html.Extract("<img class=\"showimage-house\" src=\"", "\"");
+
+            string info = html.Extract("itemprop=\"significantLink\">", "</h4>");
+            m_Prix = int.Parse(info.Extract(" - ","$").Replace(" ",""));
+            m_Precison = info.Extract("<span class=\"precision\">", "</span>");
+            if (m_Precison != null)
+                m_Precison = HttpUtility.HtmlDecode(m_Precison);
+
+            m_Summary = html.Extract("<p class=\"resultMeta\">", "</p>").RemoveHTMLTags();
+        }
+    }
+}
